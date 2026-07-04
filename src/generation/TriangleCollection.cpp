6ed1648868f54039a04b68a7c840b4e270d72a5b
Luis Eduardo Alvarez Sandoval <aluis131006@gmail.com>
2026-07-04 17:24:50 -0600

    Add warning for disconnected triangles in TriangleCollection

--- src/generation/TriangleCollection.cpp
+++ src/generation/TriangleCollection.cpp
@@ -5,6 +5,8 @@
 #include "VertexCollection.h"
 #include "TriangleCollection.h"
 
+#include <iostream>
+
 std::deque<AbstractTriangle> TriangleCollection::triangles;
 
 
@@ -25,6 +27,10 @@ int TriangleCollection::createTriangle(int v1, int v2, int v3) {
     auto tri = AbstractTriangle(v1, v2, v3);
     triangles.push_back(tri);
     int id = triangles.size() - 1;
+    if (v1 > 2 && VertexCollection::getVertex(v1)->connectingTriangles.empty() && VertexCollection::getVertex(v1)->connectingTriangles.empty() && VertexCollection::getVertex(v1)->connectingTriangles.empty()) {
+        std::cerr << "Warning: Triangle " << id << " is disconnected from the collection. Breadth-first search (BFS) is expected to fail." << std::endl;
+        VertexCollection::getVertex(0)->connectingTriangles.push_back(id);
+    }
     VertexCollection::getVertex(v1)->connectingTriangles.push_back(id);
     VertexCollection::getVertex(v2)->connectingTriangles.push_back(id);
     VertexCollection::getVertex(v3)->connectingTriangles.push_back(id);
