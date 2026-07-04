24d87764ce8e0ae5af9570404f55b18e8b6b79a3
Luis Eduardo Alvarez Sandoval <aluis131006@gmail.com>
2026-07-04 02:20:57 -0600

    Add Primitives.h header file with Ray struct definition

---
+++ src/generation/Primitives.h
@@ -0,0 +1,15 @@
+//
+// Created by Luis Alvarez on 04/07/2026.
+//
+
+#ifndef SM64_THE_MUTANT_LIMINALITY_PRIMITIVES_H
+#define SM64_THE_MUTANT_LIMINALITY_PRIMITIVES_H
+
+#include <types.h>
+
+struct Ray {
+    Vec3i origin;
+    Vec3f direction;
+};
+
+#endif // SM64_THE_MUTANT_LIMINALITY_PRIMITIVES_H
