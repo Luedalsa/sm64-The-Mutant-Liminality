985e0145a7bf4a60a74e20e96f20b82d2a5a1dc0
Luis Eduardo Alvarez Sandoval <aluis131006@gmail.com>
2026-07-08 03:25:44 -0600

    Update DebugAgent's surface transformation in AgentManager.h

--- src/generation/AgentManager.h
+++ src/generation/AgentManager.h
@@ -22,7 +22,7 @@ public:
             VertexCollection::createVertex({1, 0, 1}),
             VertexCollection::createVertex({1, 0, -1}),
             VertexCollection::createVertex({-1, 0, -1}));
-        DebugAgent ok(SurfaceTransform{0,0.5,0.5,{0,1, 0}});
+        DebugAgent ok(SurfaceTransform{0,0.5,0.5,{ 0,0.1, -0.7071}});
         agentQueue.push(&ok);
         while (!agentQueue.empty()) {
             auto agent = agentQueue.top();
