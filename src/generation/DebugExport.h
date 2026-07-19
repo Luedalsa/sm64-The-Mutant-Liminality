1ff3bbe9a5eafc60d8e6bc9184b40127a45c14c0
Luis Eduardo Alvarez Sandoval <aluis131006@gmail.com>
2026-07-18 18:28:59 -0600

    Add DebugExport.h for exporting triangles to OBJ format

---
+++ src/generation/DebugExport.h
@@ -0,0 +1,39 @@
+//
+// Created by Luis Alvarez on 18/07/2026.
+//
+
+#ifndef SM64_THE_MUTANT_LIMINALITY_DEBUGEXPORT_H
+#define SM64_THE_MUTANT_LIMINALITY_DEBUGEXPORT_H
+
+#include <fstream>
+#include <vector>
+#include "VertexCollection.h"
+#include "AbstractVertex.h"
+
+inline void exportTrianglesToObj(
+    const std::string& path,
+    const std::string& groupName = "dump"
+) {
+    std::ofstream out(path);
+    if (!out.is_open()) return;
+
+    std::unordered_map<int,int> remap;
+    std::vector<Vector3> localVerts;
+
+    for (int t = 0; t < VertexCollection::getVertexCount(); ++t) {
+        auto v = VertexCollection::getVertex(t)->position;
+        out << "v " << v.x/300.0f << " " << v.y/300.0f << " " << v.z/300.0f << "\n";
+    }
+
+    out << "g " << groupName << "\n";
+
+    for (int t = 0; t < TriangleCollection::getTriangleCount(); ++t) {
+        auto tri = TriangleCollection::getTriangle(t);
+        auto v1 = tri->getVertex(0);
+        auto v2 = tri->getVertex(1);
+        auto v3 = tri->getVertex(2);
+        out << "f " << (v1 + 1) << " " << (v2 + 1) << " " << (v3 + 1) << "\n";
+    }
+}
+
+#endif // SM64_THE_MUTANT_LIMINALITY_DEBUGEXPORT_H
