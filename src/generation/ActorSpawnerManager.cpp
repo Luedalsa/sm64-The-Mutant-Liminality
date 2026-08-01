ad5ebad2c2e346a119359c6d9e50a0061e757c33
Luis Eduardo Alvarez Sandoval <aluis131006@gmail.com>
2026-08-01 11:03:53 -0600

    REFINE: Add spawning and retrieval methods for Toads, One-Ups, Caps, and Warp Pipes in ActorSpawnerManager

--- src/generation/ActorSpawnerManager.cpp
+++ src/generation/ActorSpawnerManager.cpp
@@ -7,3 +7,9 @@
 std::vector<std::array<int, 4>> ActorSpawnerManager::doors;
 std::vector<std::array<int, 3>> ActorSpawnerManager::goombas;
 std::vector<std::array<int, 3>> ActorSpawnerManager::coins;
+std::vector<std::array<int, 5>> ActorSpawnerManager::toads;
+std::vector<std::array<int, 3>> ActorSpawnerManager::oneUps;
+std::vector<std::array<int, 3>> ActorSpawnerManager::vanishCaps;
+std::vector<std::array<int, 3>> ActorSpawnerManager::wingCaps;
+std::vector<std::array<int, 3>> ActorSpawnerManager::metalCaps;
+std::vector<std::array<int, 4>> ActorSpawnerManager::warpPipes;
